61138b13863de940195388e74b40927fe743e44a
Alexey <LESHA-31@mail.ru>
2022-12-15 16:36:53 +0500

    Update main.py

--- main.py
+++ main.py
@@ -2,31 +2,50 @@ from transformers import pipeline
 from fastapi import FastAPI
 from pydantic import BaseModel
 
+
 class Item(BaseModel):
     sequence_to_classify: str
     candidate_labels: list
 
-app = FastAPI ()
-classifier = pipeline("zero-shot-classification", model="facebook/bart-large-mnli")
+
+app = FastAPI()
+classifier = pipeline("zero-shot-classification",
+                      model="facebook/bart-large-mnli")
 
 
 @app.get("/")
 def root():
     return {"message": "Основная страница"}
 
-@app.post("/predict/")
-def predict(item: Item):
+
+@app.post("/predict_bestLable/")
+def predict_bestLable(item: Item):
+    """
+    Возвращает одну метку с лучшим счетом
+
+    Параметры:
+                    item.sequence_to_classify (str): предложение - контекст
+                    item.candidate_labels (list): набор меток
+
+    Возвращаемое значение:
+                    (dict): метка: результат (str), счет: результат (float)
+    """
     result = classifier(item.sequence_to_classify, item.candidate_labels)
-    return result['labels'], result['scores']
+    return ({"label": result["labels"][0], "score": result["scores"][0]})
 
 
+@app.post("/predict_multiLable/")
+def predict_multiLable(item: Item):
+    """
+    Возвращает все метки и их счет
 
-# classifier = pipeline("zero-shot-classification", model="facebook/bart-large-mnli")
+    Параметры:
+                    item.sequence_to_classify (str): предложение - контекст
+                    item.candidate_labels (list): набор меток
 
-# sequence_to_classify = "Я хочу поехать в Австралию"
-
-# candidate_labels = ["спорт", "путешествия", "музыка", "кино", "книги", "наука"]
-
-# result = classifier(sequence_to_classify, candidate_labels)
-
-# print(result['labels'], result['scores'])
+    Возвращаемое значение:
+                    (dict): метки: результат (list), счет: результат (list)
+    """
+    result = classifier(item.sequence_to_classify,
+                        item.candidate_labels, multi_label=True)
+    return ({"labels": result["labels"], "scores": result["scores"]})
